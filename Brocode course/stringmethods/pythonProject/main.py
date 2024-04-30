55b47850514429b73f927787f2314525863d154a
OzanBekar <ozannbekar@gmail.com>
2024-04-30 15:37:30 +0300

    Add files via upload

---
+++ Brocode course/stringmethods/pythonProject/main.py
@@ -0,0 +1,12 @@
+name = "Abigail"
+
+#print(len(name))
+#print(name.upper())
+#print(name.lower())
+#print(name.isalpha())
+#print(name.replace("i","e"))
+#print(name.find("b"))
+#print(name.capitalize())
+#print(name.count("i"))
+#print(name.isdigit())
+#print(name*5) not a method but useful feature
---
+++ Brocode course/type cast/main.py
@@ -0,0 +1,11 @@
+#type casting = convert the data type of a value to another data type
+
+x = 1
+y = 2.0
+z = "3"
+
+y = str(y)
+
+print("X is "+str(x))
+print("Y is "+y)
+print(int(z)*3)
